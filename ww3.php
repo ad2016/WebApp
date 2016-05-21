f9b3f34f61d50525f35426dbf94fa04aed466fae
sashawen <sasha_wena1@hotmail.com>
2016-05-21 15:01:28 -0700

    Add files via upload

--- ww3.php
+++ ww3.php
@@ -2,6 +2,23 @@
 <body>
 
 <?php
+//create myDB
+$servername = "localhost";
+$username = "root";
+$password = "root";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully<br>";
+$sql = "CREATE DATABASE myDB";
+if($conn->query($sql)===TRUE){
+    echo "Database created successfully";
+}else{
+    echo "Error creating database: " . $conn->error;
+}
 
 //create connection to myDB
 $servername = "localhost";
